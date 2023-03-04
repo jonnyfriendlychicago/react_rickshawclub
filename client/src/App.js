ba77737abb15776c1b9a9719565b32cd4f468302
JonFriend <jonfriend@JonFriends-MacBook-Pro.local>
2023-03-04 14:03:57 -0600

    home and rickshaw gallery added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
+
+import Header from "./Components/PageLayout/Header";
+import Home from "./Components/Verticals/Home";
+import Rickshaws from "./Components/Verticals/Rickshaws";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <BrowserRouter>
+              <Header/>
+              <Routes>
+
+                  <Route path="/" element={<Home/>} />
+
+                  <Route path="/rickshaws" element={<Rickshaws/>} />
+
+
+              </Routes>
+          </BrowserRouter>
+      </>
+
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
