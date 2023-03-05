868efd6088631e06b81acb60975741a32e7dc3ed
JonFriend <jonfriend@JonFriends-MBP.localdomain>
2023-03-05 15:58:50 -0600

    whole bunch of server stuff added, not working locally, but hoping to prove prod still works

--- client/src/App.js
+++ client/src/App.js
@@ -1,95 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link, Navigate, HashRouter} from "react-router-dom";
 
-import React from "react";
+// import React from "react";
 import Header from "./Components/PageLayout/Header";
 import Home from "./Components/Verticals/Home";
 import Rickshaws from "./Components/Verticals/Rickshaws";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import {NavItem} from "react-bootstrap";
-import {LinkContainer} from 'react-router-bootstrap'
-
 
 function App() {
   return (
       <>
-          <BrowserRouter basename="/">
-              {/*<Header/>*/}
-              <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-                  <Container >
-                      {/*<Container fluid>*/}
-                      {/*<Navbar.Brand href="/">Rickshaw Worldly</Navbar.Brand>*/}
-
-                      <LinkContainer to="/">
-                          <Navbar.Brand >Rickshaw Worldly</Navbar.Brand>
-                      </LinkContainer>
-
-                      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                      <Navbar.Collapse id="responsive-navbar-nav">
-                          <Nav className="me-auto">
-                              {/*<Nav.Link href="/rickshaws">BTS rickshaws</Nav.Link>*/}
-                              {/*<Link to={"/"}>RCT home </Link>*/}
-
-                              <LinkContainer to="/rickshaws">
-                                  <Nav.Link>Rickshaws</Nav.Link>
-                              </LinkContainer>
-
-
-
-
-
-
-
-                          </Nav>
-                          <Nav>
-                              <NavDropdown title="JonnyFriendly9" id="collasible-nav-dropdown">
-                                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                  <NavDropdown.Item href="#action/3.2">
-                                      Another action
-                                  </NavDropdown.Item>
-                                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                  <NavDropdown.Divider />
-                                  <NavDropdown.Item href="#action/3.4">
-                                      Separated link
-                                  </NavDropdown.Item>
-                              </NavDropdown>
-                          </Nav>
-                      </Navbar.Collapse>
-                  </Container>
-              </Navbar>
+          {/*<BrowserRouter basename="/">*/}
+          <HashRouter basename="/">
+              <Header/>
               <Routes>
 
                   <Route path="/" element={<Home/>} />
 
                   <Route path="/rickshaws" element={<Rickshaws/>} />
 
-
+                  {/*<redirect to="/" />*/}
+                  <Route path='*' element={<Navigate to='/' />} />
               </Routes>
-          </BrowserRouter>
+          </HashRouter>
       </>
 
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
--- client/src/Components/PageLayout/Header.js
+++ client/src/Components/PageLayout/Header.js
@@ -3,34 +3,29 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {LinkContainer} from "react-router-bootstrap";
 
 const Header = (props) => {
 
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+                {/*<Container fluid>*/}
                 <Container >
-                    {/*<Container fluid>*/}
-                    <Navbar.Brand href="/">Rickshaw World</Navbar.Brand>
+                    {/*<Navbar.Brand href="/">Rickshaw World</Navbar.Brand>*/}
+                    <LinkContainer to="/">
+                        <Navbar.Brand >Rickshaw Club</Navbar.Brand>
+                    </LinkContainer>
+
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/rickshaws">Rickshaws</Nav.Link>
-                            {/*<Nav.Link href="#pricing">Gear</Nav.Link>*/}
-                            {/*<Nav.Link href="#pricing">Community</Nav.Link>*/}
-                            {/*<Nav.Link href="#pricing">About</Nav.Link>*/}
+                            {/*<Nav.Link href="/rickshaws">Rickshaws</Nav.Link>*/}
+                            <LinkContainer to="/rickshaws">
+                                <Nav.Link>Rickshaws</Nav.Link>
+                            </LinkContainer>
 
-                            {/*<NavDropdown title="Dropdown" id="collasible-nav-dropdown">*/}
-                            {/*  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
-                            {/*  <NavDropdown.Item href="#action/3.2">*/}
-                            {/*    Another action*/}
-                            {/*  </NavDropdown.Item>*/}
-                            {/*  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
-                            {/*  <NavDropdown.Divider />*/}
-                            {/*  <NavDropdown.Item href="#action/3.4">*/}
-                            {/*    Separated link*/}
-                            {/*  </NavDropdown.Item>*/}
-                            {/*</NavDropdown>*/}
                         </Nav>
                         <Nav>
                             {/*<Nav.Link href="#deets">More deets</Nav.Link>*/}
